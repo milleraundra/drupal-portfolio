e74019a084801ba785965a3f3c2b21e55559ec25
milleraundra <miller.aundra@gmail.com>
2016-04-29 09:07:06 -0700

    add gulp task files for workflow

---
+++ sites/gulpfile.js
@@ -0,0 +1,42 @@
+
+'use strict';  // Why use strict mode? See http://www.w3schools.com/js/js_strict.asp
+
+////////////////////// Variables and Dependencies ////////////////////
+
+var gulp = require('gulp');
+var shell = require('gulp-shell');
+var browserSync = require('browser-sync').create();
+var config = require('./gulpconfig.json');
+
+
+
+//////////////////////////// Drush Commands //////////////////////////
+
+gulp.task('drushCacheClear', function() {
+  shell.task([ 'drush cc all' ]);
+  browserSync.reload();
+});
+
+
+
+////////////////////////////// Watchers ////////////////////////////
+
+gulp.task('module:watch', function() {
+  gulp.watch(config.modules, ['drushCacheClear']);
+});
+
+
+
+////////////////////// Default: Serve and Watch ////////////////////
+
+gulp.task('default', function() {
+
+  // Initiate browserSync
+  browserSync.init({
+    proxy: config.proxyUrl,
+    port: 8889
+  });
+
+  // Start Watchers
+  gulp.start('module:watch');
+});
